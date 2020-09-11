fc5e6bcea6d748986cb5352ef566a26d06d221be
mac <ganeshmani009@gmail.com>
2020-09-11 22:58:56 +0530

    Updates

---
+++ precache-manifest.b22a9d5ec772637cd3c30a5d3a443e19.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ae206ca380d9fe188bd3863e642ac3a",
+    "url": "/react-semanticui-recipes/index.html"
+  },
+  {
+    "revision": "9c020afbdc46f404b66b",
+    "url": "/react-semanticui-recipes/static/css/2.d195b239.chunk.css"
+  },
+  {
+    "revision": "b81cc331615fb37922c2",
+    "url": "/react-semanticui-recipes/static/css/main.77f5ab99.chunk.css"
+  },
+  {
+    "revision": "9c020afbdc46f404b66b",
+    "url": "/react-semanticui-recipes/static/js/2.ef1ebb08.chunk.js"
+  },
+  {
+    "revision": "5bd09eb7f2cf4d145d532d862be18942",
+    "url": "/react-semanticui-recipes/static/js/2.ef1ebb08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b81cc331615fb37922c2",
+    "url": "/react-semanticui-recipes/static/js/main.955e8700.chunk.js"
+  },
+  {
+    "revision": "10d2d7b7751aae4dad93",
+    "url": "/react-semanticui-recipes/static/js/runtime-main.8c2ae6c5.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/react-semanticui-recipes/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/react-semanticui-recipes/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/react-semanticui-recipes/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/react-semanticui-recipes/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/react-semanticui-recipes/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/react-semanticui-recipes/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/react-semanticui-recipes/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/react-semanticui-recipes/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/react-semanticui-recipes/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/react-semanticui-recipes/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/react-semanticui-recipes/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/react-semanticui-recipes/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/react-semanticui-recipes/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/react-semanticui-recipes/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/react-semanticui-recipes/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/react-semanticui-recipes/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
